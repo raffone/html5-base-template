cf14c3cbd5c06a2b6112802b94ae4addd33f6de5
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-01-23 14:58:40 +0100

    Streamlined js concatenation

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-usemin');
 
   grunt.initConfig({
 
@@ -10,18 +11,33 @@ module.exports = function(grunt) {
       dist: {
         options: {
           config: 'config.rb',
-          //importPath: '../../../../../../frameworks/rapido/stylesheets/'
+          importPath: '../../../frameworks/rapido/stylesheets/'
         }
       }
     },
 
     uglify: {
       options: {
-        mangle: false
+        mangle: {
+          except: ['jQuery']
+        },
       },
       all: {
         files: {
-          'assets/js/scripts.min.js': ['assets/js/scripts/*.js'],
+          'assets/js/ie.min.js': [
+            'assets/js/vendor/html5shiv/dist/html5shiv.js',
+            'assets/js/vendor/selectivizr.js',
+            'assets/vendor/respond/dest/respond.js',
+          ],
+          'assets/js/vendor.min.js': [
+            'assets/js/vendor/jquery/jquery.js',
+            'assets/js/vendor/bxSlider/jquery.bxslider.js',
+            'assets/js/vendor/easytabs/lib/jquery.easytabs.js',
+            'assets/js/vendor/fancybox/source/jquery.fancybox.js',
+            'assets/js/vendor/jquery-backstretch/jquery.backstretch.js',
+            'assets/js/vendor/jquery.dotdotdot/src/js/jquery.dotdotdot.js',
+            'assets/js/vendor/rapido/dist/js/rapido.js',
+          ],
           'assets/js/app.min.js': ['assets/js/app.js']
         }
       }
@@ -33,14 +49,14 @@ module.exports = function(grunt) {
         livereload: true
       },
       css: {
-        fies: ['assets/**/*.sass', 'assets/**/*.scss', 'assets/images/s/*.png' ],
+        files: ['**/*.sass', '**/*.scss', '**/*.png' ],
         tasks: ['compass']
       },
       php: {
-        files: ['**/*.php', '*.php' ]
+        files: ['*.php', '*.html', '**/*.php', '**/*.html' ]
       },
       js: {
-        files: ['assets/js/scripts/*.js', 'assets/js/app.js' ],
+        files: ['assets/js/app.js' ],
         tasks: ['uglify']
       },
     },
--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 require 'sass-globbing'
-require 'rapido-css'
+require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -10,17 +10,12 @@ images_dir = "assets/images"
 javascripts_dir = "assets/js"
 fonts_dir = "assets/fonts"
 
-output_style = :compressed
-
+output_style = :nested
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 color_output = false
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass assets/sass scss && rm -rf sass && mv scss sass
+# If you prefer the indented syntax
+preferred_syntax = :sass
